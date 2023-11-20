68a49915d433adef8ca1c616836edeccab94f551
NicolasLK <nicolas.kaminski.2001@gmail.com>
2023-11-20 16:14:28 -0300

    style: Finalizando o estilo da Page Criar campanha

--- src/pages/createCampaign.jsx
+++ src/pages/createCampaign.jsx
@@ -6,16 +6,19 @@ export default function CreateCampaign() {
   return (
     <>
       <article
-        className="w-screen
-    h-min-screen
-    rounded-tl-[15px]
-    rounded-bl-[15px]
-    p-8
-    bg-[#FFFFFF]
-    shadow-2xl
-    flex
-    justify-around">
-        <section className="flex p-1 w-full border justify-around">
+        className="w-fit
+        h-full
+        rounded-tl-[15px]
+        rounded-bl-[15px]
+        p-8
+        bg-[#FFFFFF]
+        shadow-2xl
+        flex
+        justify-center
+        border
+        my-0
+        mx-auto">
+        <section className="flex p-1 gap-4 border justify-around">
           <LastTemplates />
           <FavoritesTemplates />
         </section>
