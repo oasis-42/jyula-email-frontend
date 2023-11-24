0cb069c76298cbbc0327cac772e8b72a11bde306
EloiMatos <eloimatos00.silveira@gmail.com>
2023-11-24 14:46:25 -0300

    Adding fix in  MonacoEditor and createTemplates

--- src/components/templatesComponents/MonacoEditor.jsx
+++ src/components/templatesComponents/MonacoEditor.jsx
@@ -1,54 +1,105 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import { Box, Typography } from "@mui/material";
 
-function MonacoEditor() {
-  const [code, setCode] = useState("<!-- Digite aqui -->");
-
+function MonacoEditor({ code, onCodeChange }) {
   const editorOptions = {
     fontSize: 14,
     wordWrap: "on",
-    padding: { top: 0, bottom: 0 },
     minimap: {
       enabled: false,
     },
     language: ["html", "freemarker2"],
+    lineDecorationsWidth: "5px",
   };
 
-  const visualizationEditor = {
+  const containerStyle = {
     display: "flex",
-    flexDirection: "column",
-    flex: 1,
-  };
-  const visualizationStyle = {
-    whiteSpace: "pre-wrap",
-    overflowWrap: "break-word",
+    flexDirection: "row",
+    border: "1px solid #e0e0e0",
+    borderRadius: "5px",
     height: "72vh",
+  };
+
+  const visualizationEditorStyle = {
     display: "flex",
     flexDirection: "column",
-    overflowY: "auto",
     flex: 1,
+    maxWidth: "545px",
+  };
+
+  const visualizationStyle = {
+    overflowY: "auto",
+    overflowX: "auto",
+    width: "100%",
+    maxWidth: "545px",
+    backgroundColor: "#fff",
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    "&::-webkit-scrollbar": {
+      width: "13px",
+      height: "13px",
+    },
+    "&::-webkit-scrollbar-track": {
+      backgroundColor: "#fff",
+      borderLeft: "0.1px solid #c3c3c2",
+    },
+    "&::-webkit-scrollbar-track-piece": {
+      borderTop: "1px solid #c3c3c2",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#c3c3c2",
+      borderRadius: "0px",
+    },
+    "&::-webkit-scrollbar-thumb:hover": {
+      backgroundColor: "#929292",
+    },
+  };
+
+  useEffect(() => {
+    const emmetModule = import("emmet-monaco-es");
+
+    emmetModule.then((emmet) => {
+      emmet.setup(window.monaco);
+    });
+  }, []);
+
+  const handleEditorChange = (value) => {
+    if (onCodeChange) {
+      onCodeChange(value);
+    }
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "row", border: "1px solid #e0e0e0", borderRadius: "5px" }}>
-      <Box sx={visualizationEditor}>
-        <Typography variant="body1" sx={{ margin: "5px 0px 0px 5px" }}>
+    <Box sx={containerStyle}>
+      <Box sx={visualizationEditorStyle}>
+        <Typography variant="body2" sx={{ margin: "5px 0px 5px 10px", fontWeight: "bold", color: "#2D3C42" }}>
           HTML
         </Typography>
         <Editor
           defaultLanguage="html"
           defaultValue={code}
-          onChange={(value) => setCode(value)}
+          onChange={(value) => handleEditorChange(value)}
           options={editorOptions}
         />
       </Box>
-
-      <Box style={visualizationStyle}>
-        <Typography variant="body1" sx={{ margin: "5px 0px 0px 5px" }}>
-          Visualização
-        </Typography>
-        <div sx={visualizationStyle} dangerouslySetInnerHTML={{ __html: code }} />
+      <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <Box sx={{ boxShadow: "0px 2px 2px rgba(0, 0, 0, 0.08)", position: "sticky", top: 0 }}>
+          <Typography
+            variant="body2"
+            sx={{
+              margin: "5px 0px 5px 10px",
+              fontWeight: "bold",
+              color: "#2D3C42",
+              backgroundColor: "white",
+            }}>
+            Visualização
+          </Typography>
+        </Box>
+        <Box sx={visualizationStyle}>
+          <div dangerouslySetInnerHTML={{ __html: code }} />
+        </Box>
       </Box>
     </Box>
   );
--- src/pages/createTemplates.jsx
+++ src/pages/createTemplates.jsx
@@ -1,8 +1,53 @@
+import React, { useState } from "react";
 import MonacoEditor from "../components/templatesComponents/MonacoEditor";
-import { Box, Typography, TextField, InputAdornment, Button } from "@mui/material";
-//import { styled } from "@mui/system";
+import { Box, Typography, TextField, InputAdornment, Button, Snackbar } from "@mui/material";
+import ExampleTemplate from "../components/templatesComponents/ExampleTemplate";
+import Alert from "@mui/material/Alert";
+import axios from "axios";
 
 function CreateTemplates() {
+  const [subject, setSubject] = useState("");
+  const [code, setCode] = useState(ExampleTemplate());
+  const [apiError, setApiError] = useState(false);
+  const [emptyFieldsError, setEmptyFieldsError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSaveTemplate = async () => {
+    // Verificar se ambos os campos têm conteúdo
+    if (!subject.trim() || !code.trim()) {
+      setEmptyFieldsError(true);
+      setApiError(false);
+      setSuccess(false);
+      return;
+    }
+
+    // Simular uma requisição bem-sucedida (substitua isso com sua lógica de API real)
+    try {
+      const response = await axios.post("/api/saveTemplate", {
+        subject,
+        code,
+      });
+
+      // Aqui, após o sucesso da API, definimos o estado de sucesso como true
+      setSuccess(true);
+      setEmptyFieldsError(false);
+      setApiError(false);
+
+      console.log("Resposta da API:", response.data);
+    } catch (error) {
+      console.error("Erro ao salvar template:", error);
+      setApiError(true);
+      setEmptyFieldsError(false);
+      setSuccess(false);
+    }
+  };
+
+  const handleAlertClose = () => {
+    setEmptyFieldsError(false);
+    setApiError(false);
+    setSuccess(false);
+  };
+
   return (
     <>
       <Box sx={{ display: "flex", alignItems: "center" }}>
@@ -10,6 +55,8 @@ function CreateTemplates() {
           sx={{ width: "100%", height: "30px", margin: "14px 0px" }}
           variant="standard"
           size="small"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -19,8 +66,8 @@ function CreateTemplates() {
           }}
         />
       </Box>
-      <MonacoEditor />
-      <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: "16px" }}>
+      <MonacoEditor code={code} onCodeChange={setCode} />
+      <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: "25px" }}>
         <Button
           variant="contained"
           sx={{
@@ -29,11 +76,28 @@ function CreateTemplates() {
               backgroundColor: "#1D2C34",
             },
             textTransform: "capitalize",
-            width: "100px",
-          }}>
-          Salvar
+            width: "200px",
+          }}
+          onClick={handleSaveTemplate}>
+          <Typography>Salvar template</Typography>
         </Button>
       </Box>
+      <Snackbar
+        open={emptyFieldsError || apiError || success}
+        autoHideDuration={6000}
+        onClose={handleAlertClose}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}>
+        <Alert
+          variant="filled"
+          onClose={handleAlertClose}
+          severity={emptyFieldsError ? "warning" : apiError ? "error" : "success"}>
+          {emptyFieldsError
+            ? "Preencha tanto o Assunto quanto o Código antes de salvar."
+            : apiError
+              ? "Erro ao salvar, tente novamente mais tarde"
+              : "Template salvo com sucesso!"}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
