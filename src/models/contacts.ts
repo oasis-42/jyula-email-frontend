3c4b08e19c5cff40f9f11fe5d2f4d346e8d09e4a
JoelFrancisco <joelfranciscofilho@gmail.com>
2023-11-26 01:27:33 -0300

    finishing contact model

--- src/models/contacts.ts
+++ src/models/contacts.ts
@@ -19,3 +19,82 @@ export async function getAllContacts(filter: string = "", page: number = 1, size
   const res = await fetch(`${baseUrl}/api/v1/contacts?filter=${filter}&page=${page}&size=${size}`);
   return res.json();
 }
+
+const createOrUpdateContactSchema = contactSchema.omit({ id: true });
+
+type CreateOrUpdateContactDTO = z.infer<typeof contactSchema>;
+
+export async function createContact(dto: CreateOrUpdateContactDTO) {
+  createOrUpdateContactSchema.parse(dto);
+
+  const res = await fetch(`${baseUrl}/api/v1/contacts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dto)
+  });
+
+  if (res.status === 401) {
+    throw new Error();
+  }
+}
+
+export async function deleteContact(id: string) {
+  z.string().uuid().parse(id);
+
+  const res = await fetch(`${baseUrl}/api/v1/contacts${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
+
+  if (res.status === 401) {
+    throw new Error();
+  }
+}
+
+export async function getContactById(id: string) {
+  z.string().uuid().parse(id);
+
+  const res = await fetch(`${baseUrl}/api/v1/contacts${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
+
+  if (res.status === 401) {
+    throw new Error();
+  }
+
+  const content = res.json();
+
+  contactSchema.parse(content);
+
+  return content;
+}
+
+export async function updateContact(id: string, dto: CreateOrUpdateContactDTO) {
+  z.string().uuid().parse(id);
+  createOrUpdateContactSchema.parse(dto);
+
+  const res = await fetch(`${baseUrl}/api/v1/contacts${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dto)
+  });
+
+  if (res.status === 401) {
+    throw new Error();
+  }
+
+  const content = res.json();
+
+  contactSchema.parse(content);
+
+  return content;
+}
