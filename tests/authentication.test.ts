10218c9baf9e4a962fa5d0ad5135d362d3a94d44
unknown <lorenzotrevizoldalbo@gmail.com>
2023-11-28 23:53:20 -0300

    geração aleatoria de parametros através do faker

--- tests/authentication.test.ts
+++ tests/authentication.test.ts
@@ -1,14 +1,17 @@
 import { describe, expect, test } from "vitest";
 import { signUp } from "../src/models/authentication";
+import { faker } from "@faker-js/faker"
 
 describe("authentication", () => {
+
+
   test("should sign up a new user", async () => {
     const res = await signUp({
-      name: "joel",
-      username: "joel2",
-      password: "jesus2",
+      name: faker.person.firstName(),
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
       role: ["USER"],
-      email: "lorenzotrevizoldalbo@gmail.com",
+      email: faker.internet.email(),
     });
 
     expect(res.message).toEqual("User registered successfully!");
